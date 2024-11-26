0f36a8f6702adf44bf44bc4e652d1cdcbe35f992
gcland <grantbcopeland@gmail.com>
2024-11-26 11:33:00 -0600

    Added sums table, get sums, post sums, get results by integer input
    Added sums table, get sums, post sums, get results by integer input
    Added tests for new endpoint get results by integer input

--- app.py
+++ app.py
@@ -1,6 +1,41 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from marshmallow import fields
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
 
 app = Flask(__name__)
+ma = Marshmallow(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sum_postgres_user:ps97xpQesk6Ze8s51qR5WYsIIkI1aU9d@dpg-ct2utqu8ii6s73b2jfhg-a.oregon-postgres.render.com/sum_postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(app, model_class=Base)
+
+class Sum(Base):
+    __tablename__ = "Sum"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    num1: Mapped[int] = mapped_column(db.Integer, nullable=False)
+    num2: Mapped[int] = mapped_column(db.Integer, nullable=False)
+    result: Mapped[int] = mapped_column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Sum {self.id}: {self.num1} + {self.num2} {self.result}>'
+    
+class SumSchema(ma.Schema):
+    id = fields.Integer()
+    num1 = fields.Integer()
+    num2 = fields.Integer()
+    result = fields.Integer()
+
+sums_schema = SumSchema(many=True)
+
+@app.route('/sum', methods=['GET'])
+def find_all():
+    sums = db.session.execute(db.select(Sum)).scalars()
+    return sums_schema.jsonify(sums), 200
 
 @app.route('/sum', methods=['POST'])
 def sum():
@@ -8,4 +43,38 @@ def sum():
     num1 = data['num1']
     num2 = data['num2']
     result = num1 + num2
-    return jsonify({'result': result})
+
+    with Session(db.engine) as session:
+        with session.begin():
+            sum_entry = Sum(num1=num1, num2=num2, result=result)    #saves result and nums into database
+            session.add(sum_entry)
+
+    return jsonify({'result': result}), 201
+
+@app.route('/sum/<int:result>', methods=['GET'])
+def get_all_resultNum(result):
+    with Session(db.engine) as session:
+        with session.begin():
+            results = (db.session.execute(db.select(Sum).where(Sum.result == result)).scalars().all())
+    if results:
+        return (sums_schema.jsonify(results)), 200
+    else:
+        return jsonify({"message": "Result not found"}), 404
+
+with app.app_context():
+    # db.drop_all()
+    db.create_all()
+
+
+# Activate for testing with postman
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+# @app.route('/sum', methods=['POST'])
+# def sum():
+#     data = request.get_json()
+#     num1 = data['num1']
+#     num2 = data['num2']
+#     result = num1 + num2
+#     return jsonify({'result': result})
--- app_test.py
+++ app_test.py
@@ -6,10 +6,24 @@ class TestAPI(unittest.TestCase):
         self.app = app.test_client()
 
     def test_sum(self):
-        payload = {'num1': -2, 'num2': -4}
+        payload = {'num1': 2, 'num2': 2}
         response = self.app.post('/sum', json=payload)
         data = response.get_json()
-        self.assertEqual(data['result'], -6)
+        self.assertEqual(data['result'], 4)
+
+    def test__get_all_resultNum(self):  # this test proves the filter does find a result that does exist
+        result = 6    # ensure this result does exist within the database
+        response = self.app.get(f'/sum/{result}')
+        data = response.get_json()
+        # could also assert equal the response code, 200
+        self.assertEqual(data[0]['result'], result)     # gets the first item from the list of returned results
+
+    def test_negative_get_all_resultNum(self):  # this test proves the filter does not find a result that does not exist
+        result = 2345678976543      # ensure this result does not exist within the database
+        response = self.app.get(f'/sum/{result}')
+        data = response.get_json()
+        # could also assert equal the response code, 404
+        self.assertEqual(data['message'], 'Result not found')
 
 if __name__ == "__main__":
     unittest.main()
